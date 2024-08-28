25241071545abb0f32846fa0c9a1e65a971502da
Soumik Samanta <91832866+showMiko@users.noreply.github.com>
2024-08-29 02:16:21 +0530

    minor edit

--- src/app/components/WhatWeDo.jsx
+++ src/app/components/WhatWeDo.jsx
@@ -15,7 +15,7 @@ const WhatWeDo = () => {
               What We Do
             </h1>
           <p className="text-sm md:text-xl text-gray-50 mb-4 text-black" style={{fontStyle:'italic'}}>
-          We are the One Stop Solution for Your Products. You may choose to sell whatever you need to or can choose to buy what other's have Listed. On our platform you can be both buyer and seller. Keep Track of all your orders, customers and your listed products.All in a Seamless UI.
+          We are the One Stop Solution for Your Products. You may choose to sell whatever you need to or can choose to buy what others have Listed. On our platform you can be both buyer and seller. Keep Track of all your orders, customers and your listed products.All in a Seamless UI.
             </p>
             
         </Col>
